78a37508dd32dc867bbe7f456826e7963575d046
Hoonveloper <77804950+Hoonveloper@users.noreply.github.com>
2021-06-14 18:39:01 +0900

    Update Average.js

--- Average.js
+++ Average.js
@@ -22,6 +22,8 @@ function Average() {
             <button onClick={onInsert}>클릭</button>
             <ul>{list.map((value,index)=>(<li key={index}>{value} </li>))}</ul>
             <div>평균값: {avg}</div>
+             //원래는 <div> 평균값:{getAverage(list)}</div> 였음
+             //근데 usememo 써서 list가 변할 떄만 rendering 될 수 있도록 useMemo 사용
         </div>
     )
 }
